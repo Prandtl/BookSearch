2dd7275f19cabf629ddb628f50974b02cca61f2d
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-22 16:22:00 +0500

    created classes to Deserialize json

--- BookSearch.Core/Services/SimpleRestService.cs
+++ BookSearch.Core/Services/SimpleRestService.cs
@@ -17,10 +17,30 @@ namespace BookSearch.Core.Services
 
 		public void MakeRequest<T>(string url, string verb, Action<T> successAction, Action<Exception> errorAction)
 		{
-			var request = (HttpWebRequest) WebRequest.Create(url);
+			var request = (HttpWebRequest)WebRequest.Create(url);
 			request.Method = verb;
 			request.Accept = "application/json";
-			
+
+			MakeRequest(request,
+				response =>
+				{
+					if (successAction == null) return;
+					T toReturn;
+					try
+					{
+						toReturn = Deserialize<T>(response);
+					}
+					catch (Exception e)
+					{
+						errorAction(e);
+						return;
+					}
+					successAction(toReturn);
+				},
+				error =>
+				{
+					errorAction?.Invoke(error);
+				});
 		}
 
 		private void MakeRequest(HttpWebRequest request, Action<string> successAction, Action<Exception> errorAction)
@@ -43,7 +63,14 @@ namespace BookSearch.Core.Services
 					{
 						Mvx.Error("Error: {0} when making {1} request to {2}", e.Message, request.Method, request.RequestUri.AbsoluteUri);
 					}
-				}, null);
+				},
+				null);
+		}
+
+		private T Deserialize<T>(string responseBody)
+		{
+			var toReturn = _jsonConverter.DeserializeObject<T>(responseBody);
+			return toReturn;
 		}
 	}
 }
