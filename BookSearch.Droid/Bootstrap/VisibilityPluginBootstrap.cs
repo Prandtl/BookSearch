f16910545535e16b05f50f4f4862227c87b9e225
Vasiliy Uchanev <Vasily@sharp-dev.net>
2016-07-25 13:07:34 +0500

    progressBar

---
+++ BookSearch.Droid/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace BookSearch.Droid.Bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
+    {
+    }
+}
